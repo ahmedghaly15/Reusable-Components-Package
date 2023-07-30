065f167d37380ff8cce5c9258d90365137c7f5f6
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-07-30 19:21:04 +0300

    Update CustomIconButton Component

--- lib/src/custom_icon_button.dart
+++ lib/src/custom_icon_button.dart
@@ -7,51 +7,46 @@ class CustomIconButton extends StatelessWidget {
     required this.onTap,
     required this.iconColor,
     required this.iconSize,
-    this.offset = Offset.zero,
-    this.blurRadius = 0,
-    this.spreadRadius = 0,
-    this.shadowColor,
     this.height,
     this.width,
     this.radius,
     this.splashColor,
+    this.elevation,
+    this.boxShadow,
+    this.backgroundColor,
+    this.shape = BoxShape.rectangle,
   }) : super(key: key);
 
   final IconData icon;
   final Color iconColor;
   final double iconSize;
   final VoidCallback? onTap;
-  final Offset offset;
-  final double blurRadius;
-  final double spreadRadius;
-  final Color? shadowColor;
+  final Color? backgroundColor;
+  final BoxShape shape;
+  final List<BoxShadow>? boxShadow;
   final Color? splashColor;
   final double? height;
   final double? width;
   final double? radius;
+  final double? elevation;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height ?? 40,
-      width: width ?? 40,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(radius ?? 10),
-        boxShadow: [
-          BoxShadow(
-            offset: offset,
-            color: shadowColor ?? const Color(0xFF000000).withOpacity(0.2),
-            blurRadius: blurRadius,
-            spreadRadius: spreadRadius,
+    return Material(
+      color: Colors.transparent,
+      elevation: elevation ?? 0,
+      child: InkWell(
+        splashColor: splashColor,
+        onTap: onTap,
+        child: Container(
+          height: height ?? 40,
+          width: width ?? 40,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(radius ?? 10),
+            boxShadow: boxShadow,
+            shape: shape,
           ),
-        ],
-      ),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          splashColor: splashColor,
-          onTap: onTap,
           child: Icon(
             icon,
             color: iconColor,
