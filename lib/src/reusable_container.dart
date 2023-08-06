5965ed8a6e3063a8ff39e8a56fb80393ce324c09
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-08-06 06:45:15 +0300

    Add component

---
+++ lib/src/reusable_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReusableContainer extends StatelessWidget {
+  const ReusableContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
