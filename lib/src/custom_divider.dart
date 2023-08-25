4bf48e7660cfd02d3f33770421432b56776277b7
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-08-26 00:22:16 +0300

    Update CustomDivider

--- lib/src/custom_divider.dart
+++ lib/src/custom_divider.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class CustomDivider extends StatelessWidget {
   const CustomDivider({
     Key? key,
-    required this.height,
-    required this.width,
+    this.height,
+    this.width,
     required this.color,
   }) : super(key: key);
 
-  final double height;
-  final double width;
+  final double? height;
+  final double? width;
   final Color color;
 
   @override
