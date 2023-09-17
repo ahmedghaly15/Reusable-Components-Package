5ddb4ddda1eec659af316e452fde43c47381b029
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-09-17 07:27:24 +0300

    Rename HexColor to HexColorHandler

--- lib/src/handlers/hex_color_handler.dart
+++ lib/src/handlers/hex_color_handler.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class HexColor extends Color {
+class HexColorHandler extends Color {
   static int _getColorFromHex(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll("#", "");
     if (hexColor.length == 6) {
@@ -10,5 +10,5 @@ class HexColor extends Color {
     return int.parse(hexColor, radix: 16);
   }
 
-  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+  HexColorHandler(final String hexColor) : super(_getColorFromHex(hexColor));
 }
